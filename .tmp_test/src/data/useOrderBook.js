71c078cf09f1010796765e24f3cdfadd9760fa49
tatyna.absatarova.axelkg.com <tatyna.absatarova@axelkg.com>
2026-01-21 00:49:56 +0600

    fix: removed test tmp

--- .tmp_test/src/data/useOrderBook.js
+++
@@ -1,4 +0,0 @@
-import { useSyncExternalStore } from "react";
-export function useOrderBook(store) {
-    return useSyncExternalStore((cb) => store.subscribe(cb), () => store.getSnapshot(), () => store.getSnapshot());
-}
